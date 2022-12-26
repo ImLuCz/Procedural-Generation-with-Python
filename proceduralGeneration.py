8f1fa4e6c6081aab29e777d571aa11f03b66f7d6
LuCz <82886061+ImLuCz@users.noreply.github.com>
2022-12-26 14:36:17 +0100

    Add files via upload

---
+++ proceduralGeneration.py
@@ -0,0 +1,12 @@
+import turtle as ttl
+import random
+
+generating = True
+
+while generating == True:
+    heading = random.randint(-90, 90)
+    try:
+        ttl.setheading(heading)
+    except:
+        print("") 
+    ttl.forward(3)
